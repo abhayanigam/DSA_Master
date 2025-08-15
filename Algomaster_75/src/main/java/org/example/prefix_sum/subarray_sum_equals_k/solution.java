f40934a9af6a6394c67ce235f093e443090f791c
abhayanigam <abhaya.nigam13@gmail.com>
2025-08-15 23:33:41 +0530

    Added problems

---
+++ Algomaster_75/src/main/java/org/example/prefix_sum/subarray_sum_equals_k/solution.java
@@ -0,0 +1,76 @@
+package org.example.prefix_sum.subarray_sum_equals_k;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class solution {
+    public static void main(String[] args) {
+        System.out.println("The total number of subarrays in an array equal to K : " + getTotalSubarray(new int[]{1,1,1},2));
+        System.out.println("The total number of subarrays in an array equal to K : " + getTotalSubarray(new int[]{1,-1,0},0));
+
+//        System.out.println("The total number of subarrays in an array equal to K : " + usingTwoPointer(new int[]{1,1,1},2));
+//        System.out.println("The total number of subarrays in an array equal to K : " + usingTwoPointer(new int[]{1,2, 3},3));
+//        System.out.println("The total number of subarrays in an array equal to K : " + usingTwoPointer(new int[]{1},1));
+//        System.out.println("The total number of subarrays in an array equal to K : " + usingTwoPointer(new int[]{-1,-1,1},0));
+//        System.out.println("The total number of subarrays in an array equal to K : " + usingTwoPointer(new int[]{1,-1,0},0));
+    }
+
+    private static int[] computeSuffixSum(int[] arr) {
+        int n = arr.length;
+        int[] suffixSum = new int[n];
+
+        suffixSum[n - 1] = arr[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            suffixSum[i] = suffixSum[i + 1] + arr[i];
+        }
+        return suffixSum;
+    }
+
+        private static int getTotalSubarray(int[] nums, int k) {
+        int count = 0;
+        int currentCount = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0,1);
+
+        for (int num : nums){
+            currentCount += num;
+
+            if (map.containsKey(currentCount -k)){
+                count+=map.get(currentCount-k);
+            }
+
+            map.put(currentCount, map.getOrDefault(currentCount,0)+1);
+        }
+
+        return count;
+    }
+
+    //(Not ideal Solution) Fails for last case:
+    private static int usingTwoPointer(int[] nums, int k) {
+        if (nums.length <= 1){
+            if(nums[0] == k){
+                return k;
+            }else{
+                return 0;
+            }
+        }
+
+        int left = 0, right = left + 1, total = 0;
+
+        for (int i = 1; i <nums.length; i++) {
+            if (nums[left] + nums[right] == k){
+                total++;
+                left++;
+                right++;
+            } else if (nums[left] == k || nums[right] == k) {
+                total ++;
+            }else{
+                left++;
+                right++;
+            }
+        }
+
+        return total;
+    }
+}
